4a7516a8dc363b481c8efd3cac006c89e11516aa
Stephen Lau <stephenlau_456@hotmail.com>
2020-04-07 22:09:48 +0100

    hotfixes

--- main-consolidated.py
+++ main-consolidated.py
@@ -1,16 +1,17 @@
 import xml.etree.ElementTree as ET
 import requests
-import os
+import shutil, os
 import array
 
-endpoint = 'https://uat-store-api.priipcloud.com/product-store/api/product/'
+# obtain via WSD
+endpoint = '[endpoint scrubbed]'
 token = '[token scrubbed]'
-params = '/org/17'
+params = '[params scrubbed]'
 
 #---
 # Function which handles all the logic in relation to how the client sends us information
-#
-def operation(toUpload):
+# 
+def operation(toUpload, operation):
     
     if toUpload == 'US'
         region = 'US'
@@ -38,26 +39,67 @@ def operation(toUpload):
             if region == 'US'
                 files2.append(filename.replace("Sc9", "Sc09").replace("SEC_Shelf_", "").replace("Sc", "SEC_Shelf_Sc_").replace("_", " ").split('.')[0])
             else:
-                if docType == 'indicative':
-                    files2.append(filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", "GSSP_OC_" + docType + "_TS_EN_Sc_").replace("_", " ").split('.')[0])
+                #---
+                # slightly complicated logic is needed for gssp to get the naming convention right:
+                #   -Indicatives XML generates indicative term-sheets
+                #   -Final XMLs generates all pricing supplements AND final term-sheets
+                
+                scString = filename.split('_')[0]
+                scenarioNo = scString.split('Sc')[1]
+                
+                TSprefix = "GSSP_OC_" + docType + "_TS_EN_Sc_"
+                PSprefix = "GSSP_OC_PS_EN_Sc_"
+                
+                if int(scenarioNo) < 10:
+                    TSprefix = TSprefix + "0"
+                    PSprefix = PSprefix + "0"
+                
+                if operation == 'Full':                
+                    #This is for the indicative and final term-sheets
+                    files2.append(filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", TSprefix).replace("_", " ").split('.')[0])
+                    
+                    #extra operation for the pricing supplements
+                    if docType == 'final':
+                        files2.append(filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", PSprefix).replace("_", " ").split('.')[0])
                 else:
-                    files2.append(filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", "GSSP_OC_" + docType + "_TS_EN_Sc_").replace("_", " ").split('.')[0])
-                    files2.append(filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", "GSSP_OC_PS_EN_Sc_").replace("_", " ").split('.')[0])
-        
-        files2.sort(reverse = False) # Sort these in ascending order
+                    #This will only rename the files
+                    currDir = os.getcwd() 
+                    parDir = os.path.dirname(currDir)
+                        
+                    #This is for the indicative and final term-sheets
+                    newTSname = filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", TSprefix).replace("_", " ")
+                    os.rename(parDir + '\\' + region + '\\' + docType + '\\' + filename, parDir + '\\' + region + '\\' + docType + '\\' + newTSname)
+                    
+                    if docType == 'final':
+                        newPSname = filename.replace("_" + docType, "").replace("GSSP_OC_", "").replace("Sc", PSprefix).replace("_", " ")
+                        shutil.copytree(parDir + '\\' + region + '\\' + docType + '\\', parDir + '\\PS') # create a copy of these XML into a new directory for the PS
+                        os.rename(parDir + '\\PS\\' + filename, parDir + '\\PS\\' + newPSname)
+                
+                #---
+                # Your files are now ready to Batch-Upload in tradeforms/webapp
+                # TODO: utf-8 encoding
+                #---
+                        
+        # run this next section if it's the full operation
+        if operation == 'Full':
+            files2.sort(reverse = False) # Sort these in ascending order
 
-        #Execute api calls to upload these xmls to the product
-        for file in files:
-            idx = files.index(file)
-            tree = ET.parse('C:\\Users\\Stephen\\Desktop\\changed-' + docType + '\\' + file)
-            root = tree.getroot()
-            fileNameXMLToStr = ET.tostring(root, encoding='utf8', method='xml')
-            headersForPostRequest = {'Content-type':'application/xml', 'Authorization' : 'Bearer ' + token}
+            #Execute api calls to upload these xmls to the product
+            for file in files:
+                idx = files.index(file)
+                tree = ET.parse('C:\\Users\\Stephen\\Desktop\\changed-' + docType + '\\' + file)
+                root = tree.getroot()
+                fileNameXMLToStr = ET.tostring(root, encoding='utf8', method='xml')
+                headersForPostRequest = {'Content-type':'application/xml', 'Authorization' : 'Bearer ' + token}
 
-            urlForPostRequest = endpoint + files2[idx] + params
-            respPost = requests.post(url = urlForPostRequest, headers = headersForPostRequest, data = fileNameXMLToStr, verify=False)
+                urlForPostRequest = endpoint + files2[idx] + params
+                respPost = requests.post(url = urlForPostRequest, headers = headersForPostRequest, data = fileNameXMLToStr, verify=False)
 
 #---
-# only use this line to run the script, and state what your intended operation is
-#
-operation('Both') #Use any of the 3 following strings 'US', 'GSSP', 'Both' depending on the folders sent yby client
+# Use this line to run the script, and state what your intended operation is
+#   -Param 1: region that you would like to run the script on, permitted values are:
+#       - 'US', 'GSSP', 'Both'
+#   -Param 2: State the operation, whether you want to simply rename the files, or execute full upload operation, accepted values are:
+#       - 'Rename', 'Full'
+
+operation('Both', 'Full') 
